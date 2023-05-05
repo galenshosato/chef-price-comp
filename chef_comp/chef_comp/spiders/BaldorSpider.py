f886bc28c094fb9f141bcd5546b950b6c1d3f482
Galen Sho Sato <galen.sato@gmail.com>
2023-05-05 11:02:28 -0400

    Titles can be a little more vague

--- chef_comp/chef_comp/spiders/BaldorSpider.py
+++ chef_comp/chef_comp/spiders/BaldorSpider.py
@@ -29,20 +29,21 @@ class BaldorSpider(scrapy.Spider):
             for div in response.css('div.table-cover-back'):
                 name = div.css('div.product-title-and-sku > div.pct-heading > h3 > a::text').get()
                 name_lower = name.lower()
-                if name_lower in self.titles:
-                    item ={}
-                    price_text = div.css('div.product-title-and-sku > span.pct-price-unit > span.price::text').get()
-                    price_int = float(price_text.replace('$', '').replace(' ', '').replace('/', ''))
-                    quantity_text = div.css('div.product-title-and-sku > span.pct-price-unit > span.price > span.price-unit::text').get()
-                    numbers = re.findall(r'\d+', quantity_text)
-                    quantity_int=''
-                    for number in numbers:
-                        number = int(number)
-                        if number >= 3:
-                            quantity_int = number
-                        else:
-                            print('Still works')
-                    item['name'] = name
-                    item['price'] = price_text + quantity_text
-                    item['price per unit'] = round(price_int / quantity_int, 2)
-                    yield item
+                for title in self.titles:
+                    if title in name_lower:
+                        item ={}
+                        price_text = div.css('div.product-title-and-sku > span.pct-price-unit > span.price::text').get()
+                        price_int = float(price_text.replace('$', '').replace(' ', '').replace('/', ''))
+                        quantity_text = div.css('div.product-title-and-sku > span.pct-price-unit > span.price > span.price-unit::text').get()
+                        numbers = re.findall(r'\d+', quantity_text)
+                        quantity_int=''
+                        for number in numbers:
+                            number = int(number)
+                            if number >= 3:
+                                quantity_int = number
+                            else:
+                                print('Still works')
+                        item['name'] = name
+                        item['price'] = price_text + quantity_text
+                        item['price per unit'] = round(price_int / quantity_int, 2)
+                        yield item
